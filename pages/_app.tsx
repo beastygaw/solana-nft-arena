5c7d694757dc6dc8bc46913274ebcc906e426c3b
beastygaw <160404722+beastygaw@users.noreply.github.com>
2026-04-20 10:26:56 +0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,34 @@
-import type { AppProps } from 'next/app';
-import { useMemo } from 'react';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
-import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
-require('@solana/wallet-adapter-react-ui/styles.css');
-require('../styles/globals.css');
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import '../styles/globals.css'
+import '@solana/wallet-adapter-react-ui/styles.css'
+import type { AppProps } from 'next/app'
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets'
+import { clusterApiUrl } from '@solana/web3.js'
+import { useMemo, ReactNode } from 'react'
 
-function App({ Component, pageProps }: AppProps) {
-  const network = WalletAdapterNetwork.Devnet;
-  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+function Providers({ children }: { children: ReactNode }) {
+  const network = WalletAdapterNetwork.Devnet
+  const endpoint = useMemo(() => clusterApiUrl(network), [network])
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], [])
 
   return (
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets} autoConnect={false}>
         <WalletModalProvider>
-          <Component {...pageProps} />
+          {children}
         </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
-  );
+  )
 }
 
-export default App;
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Providers>
+      <Component {...pageProps} />
+    </Providers>
+  )
+}
