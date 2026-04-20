ae86061263a82e47003aa491735abce1e68ada22
beastygaw <160404722+beastygaw@users.noreply.github.com>
2026-04-20 10:46:24 +0500

    Update and rename _app.tsx to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,13 @@
 import '../styles/globals.css'
 import '@solana/wallet-adapter-react-ui/styles.css'
-import type { AppProps } from 'next/app'
+import { useMemo } from 'react'
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets'
 import { clusterApiUrl } from '@solana/web3.js'
-import { useMemo } from 'react'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }) {
   const network = WalletAdapterNetwork.Devnet
   const endpoint = useMemo(() => clusterApiUrl(network), [network])
   const wallets = useMemo(() => [new PhantomWalletAdapter()], [])
